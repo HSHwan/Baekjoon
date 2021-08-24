40ab4e884c154e960c3283db21d88113d3d9c432
HSHwan <tnghks2866@gmail.com>
2021-08-24 17:52:55 +0900

    3 data structure problem solved

---
+++ 1620_PoketmonMaster.cpp
@@ -0,0 +1,29 @@
+// Data Structure - Map
+#include <iostream>
+#include <map>
+#include <string>
+using namespace std;
+
+int N,M;
+string poketmon, problem;
+string poketmon_book_name[100001];
+map<string, int> poketmon_book_num;
+
+int main() {
+    ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+    cin >> N >> M;
+    for (int i = 0; i < N; i++){
+        cin >> poketmon;
+        poketmon_book_name[i+1] = poketmon;
+        poketmon_book_num[poketmon] = i+1;
+    }
+    for (int i = 0; i < M; i++){
+        cin >> problem;
+        if(problem[0] >= 'A' && problem[0] <= 'Z'){
+            cout << poketmon_book_num[problem] << '\n';
+        }
+        else {
+            cout << poketmon_book_name[stoi(problem)] << '\n';
+        }
+    }
+}
