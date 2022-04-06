c6c160ca46dceeefd78151942b2800e5af76e332
HSHwan <86449722+HSHwan@users.noreply.github.com>
2022-04-06 23:14:09 +0900

    Partial Sum problem solved

---
+++ 24552_CorrectParenthesis.cpp
@@ -0,0 +1,28 @@
+// Partial Sum
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main(){
+    string parse_str;
+    cin >> parse_str;
+
+    int leftCnt = 0,  rightCnt = 0, parseSum = 0;
+    for (int i = 0; i < parse_str.length(); i++){
+        if (parse_str[i] == '('){
+            leftCnt++;
+            parseSum++;
+        } else if (parse_str[i] == ')'){
+            rightCnt++;
+            parseSum--;
+        }
+        if (parseSum == 0){
+            leftCnt = 0;
+        } else if (parseSum < 0){
+            cout << rightCnt;
+            break;
+        }
+    }
+    if (parseSum > 0)   cout << leftCnt;
+}
