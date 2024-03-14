f2cc266df1dbe476a34c08bcdff89a159ac22d3f
HSHwan <tnghks2866@gmail.com>
2024-03-14 13:23:23 +0900

    Refactor: 자료구조 스택을 통해 시간 단축 및 코드 간결화

--- 16120_PPAP.cpp
+++ 16120_PPAP.cpp
@@ -1,61 +1,39 @@
-// String
+// Stack
 #include <iostream>
 #include <string>
+#include <stack>
+
 using namespace std;
 
 int main() {
     ios::sync_with_stdio(false), cin.tie(NULL), cout.tie(NULL);
     string PPAP;
-    int check = 0; // 1 : 1st P, 2: 2nd P, 3: 3rd A, 4: 4th P --> 1 or 2 ( previous P exists ) 
-    int P_count = 0;
+    bool clearPPAP = false;
     cin >> PPAP;
 
-    int i;
-    int PPAP_len = PPAP.length();
+    stack<char> PPAPStack;
 
-    if (PPAP_len == 1 && PPAP[0] == 'P')
-        cout << "PPAP";
-    else if (PPAP[0] == 'A')
-        cout << "NP";
-    else {
-        for (i = 0; i < PPAP_len; i++) {
-            if (check == 1 && PPAP[i] == 'A') {
-                P_count = -1;
-                break;
-            }
-
-            if (PPAP[i] == 'P')
-                P_count++;
-
-            if (check == 0 && PPAP[i] == 'P') {
-                check = 1;
-                continue;
-            }
-
-            if (check == 1 && PPAP[i] == 'P') {
-                check = 2;
-                continue;
-            }
-
-            if (check == 2 && PPAP[i] == 'A') {
-                check = 3;
-                continue;
-            }
- 
-            if (check == 3 && PPAP[i] == 'P') {
-                check = 4;
-                P_count -= 3;
-                P_count++;
-                if (P_count > 1)
-                    check = 2;
-                else 
-                    check = 1;
-            }
+    for (const char& PPAPChar : PPAP){
+        PPAPStack.push(PPAPChar);
+        if (clearPPAP && PPAPStack.top() == 'P' && PPAPStack.size() >= 4){
+            for (int i = 0; i < 2; i++)
+                PPAPStack.pop();
+            if (PPAPStack.top() == 'P' && PPAPStack.size() >= 2)
+                PPAPStack.pop();
+            else    break;
+            clearPPAP = false;
         }
-        if (P_count == 1)
-            cout << "PPAP";
-        else
-            cout << "NP";
+        else if (clearPPAP)
+            break;
+
+        if (PPAPChar == 'A')
+            clearPPAP = true;
     }
+
+    if (PPAPStack.top() == 'P' && PPAPStack.size() == 1)
+        cout << "PPAP";
+    else
+        cout << "NP";
+
     return 0;
 }
