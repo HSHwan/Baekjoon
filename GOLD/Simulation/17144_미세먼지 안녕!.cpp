55b75fff71f5cd7138e7108eb7cfce6bd24d22fd
HSHwan <tnghks2866@gmail.com>
2024-03-26 17:23:15 +0900

    Modify: 17144번 미세먼지 안녕! [G4 / Simulation]

--- GOLD/Simulation/17144_미세먼지 안녕!.cpp
+++ GOLD/Simulation/17144_미세먼지 안녕!.cpp
@@ -6,19 +6,19 @@
 using namespace std;
 
 int houseRow, houseColumn;
-int airTime;
+int workTime;
 vector<vector<int>> houseDust;
-vector<pair<int, int>> airCleaner;
+vector<int> airCleaner;
 
 void input(){
-    cin >> houseRow >> houseColumn >> airTime;
+    cin >> houseRow >> houseColumn >> workTime;
     vector<vector<int>>(houseRow, vector<int>(houseColumn)).swap(houseDust);
 
     FOR(i, houseRow){
         FOR(j, houseColumn){
             cin >> houseDust[i][j];
             if (houseDust[i][j] == -1)
-                airCleaner.push_back({i, j});
+                airCleaner.push_back(i);
         }
     }
 }
@@ -26,7 +26,7 @@ void input(){
 bool isSpreadable(const pair<int, int>& pos){
     if (pos.X >= houseRow || pos.Y >= houseColumn || pos.X < 0 || pos.Y < 0)
         return false;
-    return !(pos == airCleaner[0] || pos == airCleaner[1]);
+    return houseDust[pos.X][pos.Y] != -1;
 }
 
 void spreadDust(vector<vector<int>>& map, const pair<int, int>& pos, const int& posDust){
@@ -46,8 +46,8 @@ void spreadDust(vector<vector<int>>& map, const pair<int, int>& pos, const int& 
 
 void spreadDustAll(){
     vector<vector<int>> dustSpreaded(houseRow, vector<int>(houseColumn, 0));
-    dustSpreaded[airCleaner[0].X][0] = -1;
-    dustSpreaded[airCleaner[1].X][0] = -1;
+    dustSpreaded[airCleaner[0]][0] = -1;
+    dustSpreaded[airCleaner[1]][0] = -1;
     FOR(i, houseRow){
         FOR(j, houseColumn){
             if (houseDust[i][j] == -1 || !houseDust[i][j])
@@ -58,34 +58,34 @@ void spreadDustAll(){
     houseDust = dustSpreaded;
 }
 
-void circulateDust(){
-    // circulate counterClockWise
-    FOR(i, airCleaner[0].X - 1)
-        houseDust[i + 1][0] = houseDust[i][0];
+void workAirCleaner(){
+    // Circulate counterclockwise
+    for (int i = airCleaner[0] - 1; i > 0; i--)
+        houseDust[i][0] = houseDust[i - 1][0];
     FOR(i, houseColumn - 1)
         houseDust[0][i] = houseDust[0][i + 1];
-    FOR(i, airCleaner[0].X)
+    FOR(i, airCleaner[0])
         houseDust[i][houseColumn - 1] = houseDust[i + 1][houseColumn - 1];
     for (int i = houseColumn - 1; i > 1; i--)
-        houseDust[airCleaner[0].X][i] = houseDust[airCleaner[0].X][i - 1];
-    houseDust[airCleaner[0].X][1] = 0;
+        houseDust[airCleaner[0]][i] = houseDust[airCleaner[0]][i - 1];
+    houseDust[airCleaner[0]][1] = 0;
 
-    // circulate clockWise
-    for (int i = airCleaner[1].X + 1; i < houseRow - 1; i++)
+    // Circulate clockwise
+    for (int i = airCleaner[1] + 1; i < houseRow - 1; i++)
         houseDust[i][0] = houseDust[i + 1][0];
     FOR(i, houseColumn - 1)
         houseDust[houseRow - 1][i] = houseDust[houseRow - 1][i + 1];
-    for (int i = houseRow - 1; i > airCleaner[1].X; i--)
+    for (int i = houseRow - 1; i > airCleaner[1]; i--)
         houseDust[i][houseColumn - 1] = houseDust[i - 1][houseColumn - 1];
     for (int i = houseColumn - 1; i > 1; i--)
-        houseDust[airCleaner[1].X][i] = houseDust[airCleaner[1].X][i - 1];
-    houseDust[airCleaner[1].X][1] = 0;
+        houseDust[airCleaner[1]][i] = houseDust[airCleaner[1]][i - 1];
+    houseDust[airCleaner[1]][1] = 0;
 }
 
 void solve(){
-    FOR(i, airTime){
+    FOR(i, workTime){
         spreadDustAll();
-        circulateDust();
+        workAirCleaner();
     }
 }
 
