72e93725ed7e7f9cbf26aeea56a91c5430aa83cf
HSHwan <tnghks2866@gmail.com>
2025-02-24 18:06:00 +0900

    solve: 11727번 2xn 타일링 2 [S3 / DP]

---
+++ SILVER/DP/11727_2xn 타일링 2.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main(){
+    int n;
+    cin >> n;
+    vector<int> tiles(n + 1, 0);
+    tiles[1] = 1;
+    tiles[2] = 3;
+
+    for (int i = 3; i <= n; i++) {
+        tiles[i] = (tiles[i - 1] + 2 * tiles[i - 2]) % 10007;
+    }
+
+    cout << tiles[n];
+}
